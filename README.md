f90e472c515c0e6de9f9e8b4874954a8a57c71d4
wally@ <wally@>
2023-11-22 23:24:49 -0800

    sync z@d@h1: update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # zs-state
+
+
+The goal of this package is to provide a unified abstraction for all state/persistence needs so that the backend can be proxied in a way that is usable for Pyodide deployments.
+
+This allows environments such as Shinylive to operate over a ZeroServer data mesh and have access to persistence in a pluggable way.
---
+++ src/python/zeroserver_state/__init__.py
@@ -0,0 +1,38 @@
+class PubSubEventType(Enum):
+    
+
+class NotificationHandler:
+
+    await def handle_pubsub(self,
+        pubsub_key: str,
+        pubsub_event_type: PubSubEventType,
+        pubsub_data: dict) -> None: pass
+
+class PersistenceHandle:
+    """
+
+    messaging encryption, underlying backend (Supabase, Ethereum, etc.) and setup (login, auth, password, etc.) should be abstrcated behind this class, such that once an app has an initialized version of this object, it is assumed that everything needed to set that up has already magically happened in the backgroun
+
+    in this system, there is only pubsub.  a messaging setup is just a two party pubsub
+
+    """
+
+
+    await def block_read(self,position,length) -> byte[]: pass
+    await def block_write(self,position,length,data) -> None: pass
+
+    await def pubsub_send(self,pubsub_key,data,callback_fn) -> None: pass
+    await def pubsub_subscribe(self,
+        pubsub_key: str,
+        notification_handler: NotificationHandler) -> None: pass
+
+    await def pubsub_unsubscribe(self,
+        pubsub_key: str,
+        notification_handler: NotificationHandler) -> None: pass
+
+    await def get_local_kvstore(self) -> dict: 
+        """used to get the local version of the key-value store used for things like data that needs to go into the UI
+        because the dict does not have a notification mechansim, a Pubsub should be used instead of polling to notify when to process new information
+        """
+        pass
+
